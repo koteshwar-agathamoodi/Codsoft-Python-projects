64e9cf69d03621d60c2912a2f4f14f1fe405f928
koteshwar-agathamoodi <154686458+koteshwar-agathamoodi@users.noreply.github.com>
2023-12-24 14:16:52 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,32 @@
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    print("Choose an operation:")
+    print("-------------------")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    operation = input("Enter your choice : ")
+
+    if operation == "1":
+        ans = num1 + num2
+        print(f"The result of addition is: {ans}")
+    elif operation == "2":
+        ans = num1 - num2
+        print(f"The result of subtraction is: {ans}")
+    elif operation == "3":
+        ans = num1 * num2
+        print(f"The result of multiplication is: {ans}")
+    elif operation == "4":
+        if num2 == 0:
+            print("Cannot divide by zero!")
+        else:
+            result = num1 / num2
+            print(f"The result of division is: {ans}")
+    else:
+        print("Invalid choice, Please try again.")
+
+calculator()
